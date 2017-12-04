ce21a23e1db4b0a7cad2f419d5d30a71dbc119be
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-04 09:33:27 +0000

    Expected { after 'if' condition. (curly) #13

--- classifiers/neural/NeuralNetwork.js
+++ classifiers/neural/NeuralNetwork.js
@@ -10,8 +10,9 @@ var NeuralNetwork = require('brain').NeuralNetwork;
 NeuralNetwork.prototype.trainOnline = function () {throw new Error("NeuralNetwork does not support online training");}; 
 NeuralNetwork.prototype.trainBatch  = function(dataset) {
 	dataset.forEach(function(datum) {
-		if (!Array.isArray(datum.output) && !(datum.output instanceof Object))
+		if (!Array.isArray(datum.output) && !(datum.output instanceof Object)){
 			datum.output = [datum.output];
+		}
 	});
 	this.train(dataset); 
 };
